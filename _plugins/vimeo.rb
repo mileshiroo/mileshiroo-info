7dce49d58a6917d8f5800bfe554c587d484cd2fb
Miles Peyton <milespeyton@gmail.com>
2016-03-20 19:05:09 +0800

    something?

--- _plugins/vimeo.rb
+++ _plugins/vimeo.rb
@@ -1,6 +1,3 @@
-# A plugin for embedding videos from Vimeo using a simple Liquid tag, ie: {% vimeo 12345678 %}.
-# Based of the Youtube plugin from http://www.portwaypoint.co.uk/jekyll-youtube-liquid-template-tag-gist/
-
 module Jekyll
   class Vimeo < Liquid::Tag
     def initialize(name, id, tokens)
